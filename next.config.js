c3ae693a2291e832bdba2a38df34a91c385c33c8
Sri Kanth V <srikanth200428@gmail.com>
2026-03-31 13:46:42 +0530

    remove basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/Portfolio_new',
-  assetPrefix: '/Portfolio_new/',
   output: 'export',
   images: {
     unoptimized: true,
