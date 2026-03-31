2d01392ea6c37ffd9d6ccd794c7d0f7e84c35ec6
Sri Kanth V <srikanth200428@gmail.com>
2026-03-31 13:07:52 +0530

    fix: add basePath for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/Portfolio_new',
+  assetPrefix: '/Portfolio_new/',
   images: {
     unoptimized: true,
   },
